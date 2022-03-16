b4c337b602432f7f2a77239004e199df58fb1364
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-03-16 01:29:19 -0400

    :label: (core) introduce types/interfaces modules

---
+++ core/interfaces.ts
@@ -0,0 +1,6 @@
+import { platforms } from 'call-of-duty-api';
+
+export interface Player {
+  gamertag: string;
+  platform: platforms;
+}
---
+++ core/types.ts
@@ -0,0 +1,11 @@
+export type CareerResponse = {
+  kills?: number;
+  wins?: number;
+  kdRatio?: number;
+  deaths?: number;
+};
+
+export type HighlightsResponse = {
+  mostKills?: number,
+  highestKD?: number
+};
