4aba07cfe04155ebba17f364d7555095ea082151
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-03-25 20:36:36 -0400

    :sparkles: (pages/api) refactor human readable format to its own category

--- pages/api/rebirth.ts
+++ pages/api/rebirth.ts
@@ -2,6 +2,12 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 import { getRebirth } from "../../core";
 
+enum Formats {
+  json = "json",
+  text = "text",
+  human = "human"
+}
+
 const getRebirthHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { body, query } = req;
 
@@ -11,11 +17,25 @@ const getRebirthHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   
   const stats = await getRebirth({ gamertag, platform });
   
-  const format = body.format || query.format;
-  const response = format === "text"
-    ? `Most Kills of the day: ${stats.mostKills}, Highest KD of the day: ${stats.highestKD}`
-    : stats;
-  res.send(response);
+  const sentFormat = body.format || query.format;
+  const format = sentFormat || Formats.json;
+  switch (format) {
+    case Formats.json: {
+      res.send(stats);
+      return;
+    }
+    case Formats.text: {
+      res.send(JSON.stringify(stats));
+      return;
+    }
+    case Formats.human: {
+      res.send(`Most Kills of the day: ${stats.mostKills}, Highest KD of the day: ${stats.highestKD}`);
+      return;
+    }
+    default:
+      res.send(stats);
+      return;
+  }
 };
 
 export default getRebirthHandler;
