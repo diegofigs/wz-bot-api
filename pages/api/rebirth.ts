c23f56606fdd173c2f66fadea7e0890486b47427
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-03-25 20:11:48 -0400

    :sparkles: (pages/api) add text option to rebirth route

--- pages/api/rebirth.ts
+++ pages/api/rebirth.ts
@@ -1,12 +1,21 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 
-import { getRebirth } from '../../core';
+import { getRebirth } from "../../core";
 
 const getRebirthHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { gamertag, platform } = req.body;
+  const { body, query } = req;
 
+  const isPOST = req.method === "POST";
+  const gamertag = isPOST ? body.gamertag : query.gamertag;
+  const platform = isPOST ? body.platform : query.platform;
+  
   const stats = await getRebirth({ gamertag, platform });
-  res.send(stats);
+  
+  const format = body.format || query.format;
+  const response = format === "text"
+    ? `Most Kills of the day: ${stats.mostKills}, Highest KD of the day: ${stats.highestKD}`
+    : stats;
+  res.send(response);
 };
 
 export default getRebirthHandler;
