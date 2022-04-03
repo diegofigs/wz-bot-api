b36aecadc0d7580ea3bc133d938ebb50699b622e
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-04-03 15:03:53 -0400

    :sparkles: (pages/api) add players option for bulk rebirt…

--- pages/api/rebirth.ts
+++ pages/api/rebirth.ts
@@ -2,37 +2,67 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { platforms } from "call-of-duty-api";
 import { startOfDay } from "date-fns";
 
-import { getRebirth } from "core";
-import { HighlightsResponse, Formats } from "core/types";
+import { getRebirth, getRebirthBulk } from "core";
+import { HighlightsResponse, Formats, LeaderboardResponse } from "core/types";
+import { Player } from "core/interfaces";
 
 const allowedMethods = ["GET", "POST"];
-const getRebirthHandler = async (req: NextApiRequest, res: NextApiResponse<HighlightsResponse | string>) => {
+const getRebirthHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<LeaderboardResponse | HighlightsResponse | string>
+) => {
   const { method } = req;
   if (allowedMethods.includes(method)) {
     const { body, query } = req;
     const gamertag = (body.gamertag || query.gamertag) as string;
     const platform = (body.platform || query.platform) as platforms;
-    if (!gamertag || !platform) {
-      res.status(400).end('gamertag and platform are required arguments');
+    const players = body.players;
+
+    if (!players && (!gamertag || !platform)) {
+      res.status(400).end("gamertag and platform are required arguments");
+      return;
+    }
+
+    if (players) {
+      const isValidFormat = players.every((p: Player) => p.gamertag && p.platform);
+      if (isValidFormat) {
+        const stats = await getRebirthBulk(players);
+        res.send(stats);
+      } else {
+        res
+          .status(400)
+          .end(
+            `"players" is invalid, ensure all entries include a gamertag and platform`
+          );
+      }
       return;
     }
 
     const responseFormat = query.format as string;
     const format = responseFormat || Formats.json;
     if (!Object.values(Formats).includes(format as Formats)) {
-      res.status(400).end(`"${format}" is not a valid format, use <${Formats.json}|${Formats.text}|${Formats.human}> instead`);
+      res
+        .status(400)
+        .end(
+          `"${format}" is not a valid format, use <${Formats.json}|${Formats.text}|${Formats.human}> instead`
+        );
       return;
     }
 
     try {
       const now = new Date();
-      const interval = { start: startOfDay(now).getUTCSeconds(), end: now.getUTCSeconds() };
+      const interval = {
+        start: startOfDay(now).getUTCSeconds(),
+        end: now.getUTCSeconds(),
+      };
       const stats = await getRebirth({ gamertag, platform }, interval);
       if (format === Formats.json) {
         res.send(stats);
       }
       if (format === Formats.text || format === Formats.human) {
-        res.send(`Most Kills of the day: ${stats.mostKills}, Highest KD of the day: ${stats.highestKD}`);
+        res.send(
+          `Most Kills of the day: ${stats.mostKills}, Highest KD of the day: ${stats.highestKD}`
+        );
       }
     } catch (error) {
       res.status(500).end(`Error getting Rebirth highlights for ${gamertag}`);
