92869a80680ee14a4d2f17a26b7948d0c4a6fdca
Diego Figueroa <12281609+diegofigs@users.noreply.github.com>
2022-07-15 01:27:47 +0000

    refactor: add runtime validation to /stats, fix type errors

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -2,28 +2,30 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { platforms } from 'call-of-duty-api';
 
 import { getStats } from 'core';
+import { Player } from 'core/types';
 
 const allowedMethods = ["GET", "POST"];
 const getStatsHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req;
-  if (allowedMethods.includes(method)) {
-    const { body, query } = req;
-    const gamertag = (body.gamertag || query.gamertag) as string;
-    const platform = (body.platform || query.platform) as platforms;
-    if (!gamertag || !platform) {
-      res.status(400).end('gamertag and platform are required arguments');
-      return;
-    }
+  if (method && !allowedMethods.includes(method)) {
+    return res
+      .setHeader("Allow", allowedMethods)
+      .status(405)
+      .end(`Method ${method} Not Allowed`);
+  }
+
+  const { body, query } = req;
+  try {
+    const player = Player.parse(method === 'POST' ? body : query);
+    const { gamertag, platform } = player;
     try {
       const stats = await getStats({ gamertag, platform });
-      res.send(stats);
+      return res.send(stats);
     } catch (error) {
-      res.status(500).end(`Error getting stats for ${gamertag}`);
+      return res.status(500).end(`Error getting stats for ${gamertag}`);
     }
-    
-  } else {
-    res.setHeader("Allow", allowedMethods);
-    res.status(405).end(`Method ${method} Not Allowed`);
+  } catch (error) {
+    return res.status(400).end('gamertag and platform are required arguments');
   }
 };
 
