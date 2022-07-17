459bf32766cadc05e23b31c27420428f75bec653
Diego Figueroa <12281609+diegofigs@users.noreply.github.com>
2022-07-17 11:37:15 +0000

    fix: :rotating_light: add types to api return values

--- functions/src/api.ts
+++ functions/src/api.ts
@@ -5,6 +5,17 @@ const API = "https://wz-bot-api-e6rusiaura-ue.a.run.app/api";
 
 const headers = { "Content-Type": "application/json" };
 
+export type HighlightsResponse = {
+  mostKills: number,
+  highestKD: number,
+  gamertag?: string
+};
+
+export type LeaderboardResponse = {
+  byKills: HighlightsResponse[],
+  byKDR: HighlightsResponse[]
+}
+
 export const getRebirthBulk = async () => {
   try {
     const response = await request({
@@ -13,7 +24,7 @@ export const getRebirthBulk = async () => {
       headers,
       data: { players },
     });
-    return response.data;
+    return response.data as LeaderboardResponse;
   } catch (error) {
     console.error(error);
     return { byKills: [], byKDR: [] };
